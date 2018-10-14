cfe578e8c722fb71cc0e206aa50e8537ffdd0526
vwjames <vwjames@sfu.ca>
2018-10-14 04:11:10 -0700

    Ui layout complete

--- ATD/Assets/Scripts/UI.cs
+++ ATD/Assets/Scripts/UI.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI : MonoBehaviour {
+	public void Resume(){
+		Time.timeScale = 1f;
+	}
+	public void Pause(){
+		Time.timeScale = 0f;
+	}
 	public void Title(){
 		SceneManager.LoadScene("Title_Page");
 	}
