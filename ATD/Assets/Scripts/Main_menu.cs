826b6decc248480762a8da4b8e5cbdf7c186125c
vwjames <vwjames@sfu.ca>
2018-10-13 20:45:36 -0700

    Inter-scene navigation

---
+++ ATD/Assets/Scripts/Main_menu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_menu : MonoBehaviour {
+	public void Play(){
+		SceneManager.LoadScene("Level_Select");
+	}
+	public void Quit(){
+		Application.Quit();
+	}
+}
