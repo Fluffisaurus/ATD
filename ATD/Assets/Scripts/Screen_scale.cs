22ccf30f0894fe52297837f4b3be2f91a59fcafc
vwjames <vwjames@sfu.ca>
2018-10-14 23:33:34 -0700

    Set scale

---
+++ ATD/Assets/Scripts/Screen_scale.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screen_scale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution( 2400, 1400, false );
+	}
+
+}
