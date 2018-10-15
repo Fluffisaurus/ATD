3f962c47015b6c2647349a898e478ca53fe9b6cd
Fluffisaurus <angushon96@gmail.com>
2018-10-14 17:27:42 -0700

    can't get level and wave number to display correctly

---
+++ ATD/Assets/PlayerLevelUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerLevelUI : MonoBehaviour {
+
+    public Text level;
+    public Text wave;
+
+    private int difficulty = 0;
+    private int currLevel = 1;
+
+    private void Update() {
+        if (Difficulty.difficulty[0] == true) level.text = "Level: " + currLevel + " (EASY)";
+        else if (Difficulty.difficulty[1] == true) level.text = "Level: " + currLevel + " (MEDIUM)";
+        else if (Difficulty.difficulty[2] == true) level.text = "Level: " + currLevel + " (HARD)";
+        else level.text = "Level: " + currLevel + " (EASY)";
+
+        currLevel = Menu.currentLevel;
+        for (int i = 0; i < Difficulty.difficulty.Length; i++) Difficulty.difficulty[i] = false;
+    }
+
+
+}
