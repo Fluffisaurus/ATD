583ef5a2de8a93dbdf8ce06595016105386c8a4b
vwjames <vwjames@sfu.ca>
2018-10-14 19:49:05 -0700

    Added victory screen

---
+++ ATD/Assets/Scripts/Victory_UI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory_UI : MonoBehaviour {
+
+	public void Victory_retry(){
+		SceneManager.LoadScene("Difficulty_Select");
+	}
+	public void Victory_Quit(){
+		Application.Quit();
+	}
+
+}
